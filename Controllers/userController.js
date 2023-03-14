bc4c4ecf3d707ccc0889a1a4ea5ba35adae59b7c
Julianfmv <julianfmv@gmail.com>
2023-03-14 19:38:49 -0300

    PostUser Route and controller & db connection name deleted

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 
 const getUsers = async () => {
     try {
-        const users = await User.find({})
+        const users = await User.find()
         if (!users.length) throw new Error("No users found in DB") 
         return users;
     } catch (error) {
@@ -11,4 +11,18 @@ const getUsers = async () => {
     }
 }
 
-module.exports = { getUsers }
+const postNewUser = async (user) => {
+    try {
+        if(!user.first_name || !user.password || !user.email) throw new Error("Falta informacion acerca del usuario");
+        const newUser = new User(user)
+        newUser.save(newUser)
+        return newUser
+        
+    } catch (error) {
+        return error.message;
+    }
+}
+
+module.exports = { getUsers, postNewUser}
+
+
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { getUsers } = require('../Controllers/userController')
+const { getUsers, postNewUser } = require('../Controllers/userController')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
@@ -12,5 +12,18 @@ router.get('/', async (req, res, next) => {
     return res.status(400).send(error)
   }
 });
+
+router.post('/', async (req, res, next) => {
+  try {
+
+    const newUser = await postNewUser(req.body);
+    if(newUser.error) throw new Error(newUser.error);
+    return res.status(201).json(newUser);
+
+  } catch (error) {
+    return res.status(400).send(error)
+    
+  }
+})
 
 module.exports = router;
