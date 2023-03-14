1727b358f3ebac430be9c3bb03569d6159abf84a
unknown <tomi_877@hotmail.com>
2023-03-14 18:27:39 -0300

    userController

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const { getUsers } = require('../Controllers/userController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await getUsers();
+    if(users.error) throw new Error(users.error)
+    return res.status(200).json(users)
+  } catch (error) {
+    return res.status(400).send(error)
+  }
 });
 
 module.exports = router;
