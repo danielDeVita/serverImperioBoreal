661b688652b342ab4a77865fc0446bc3a6e94c9f
unknown <tomi_877@hotmail.com>
2023-03-15 17:03:12 -0300

    arreglo

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -2,8 +2,8 @@ const Product = require('../models/Product')
 
 const getProducts = async() => {
     try {
-        const products = await product.find()
-        if(!products.lenght)
+        const products = await Product.find()
+        if(!products.length)
         throw new Error("No se encontraron productos en la base de datos")
         return products;
     } catch (error) {
