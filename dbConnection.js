63bb9b4e5832a92319f296499c9aae5f0f3b70d4
Julianfmv <julianfmv@gmail.com>
2023-03-19 13:32:30 -0300

    broken db connection, halfway done with cloudinary + multer

--- dbConnection.js
+++ dbConnection.js
@@ -1,12 +1,32 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const CLUSTER = process.env.CLUSTER
+const CLOUD_NAME = process.env.CLOUD_NAME;
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-module.exports = async function connect() {
+cloudinary.config({
+    cloud_name: CLOUD_NAME,
+    api_key: API_KEY,
+    api_secret: API_SECRET,
+})
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'Products',
+        allowed_formats: ['jpg', 'jpeg', 'png']
+    }
+})
+
+const upload = multer({storage:storage})
+
+const connect = async () => {
     try {
         await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${CLUSTER}.mongodb.net/ImperioBoreal`, {
             useNewUrlParser: true,
@@ -16,5 +36,11 @@ module.exports = async function connect() {
     }
     catch (error) {
         console.log(error);
-    }
+        }
+}
+
+
+module.exports = {
+    connect,
+    upload: upload 
 }
