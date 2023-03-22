5dd2b014306729b746d69e59c16c45e804ff5b4c
danielDeVita <danielitodevita@gmail.com>
2023-03-22 16:10:36 -0300

    manejo de error si faltan parametros tryCatch

--- routes/carts.js
+++ routes/carts.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const { addProductToCart } = require("../Controllers/addProductToCart");
 
 router.post("/", async (req, res) => {
-    const cart = await addProductToCart(req.body)
-    return res.status(201).json(cart);
-})
+    try {
+        const newCart = await addProductToCart(req.body) //de donde sacamos user (req.params.id?) y producs??
+        if (newCart.error) throw new Error(cart.error);
+        return res.status(201).json(newCart);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+});
 
 module.exports = router;
