d08d0dd5279b51b7b1de0467253b6ae29abf2d03
Julianfmv <julianfmv@gmail.com>
2023-03-28 12:57:42 -0300

    reviews crud done and working

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Review = require('../models/Review')
 const mongoose = require('mongoose')
-const {getAllReviews, createReview, getReviewsByProduct} = require('../Controllers/reviewController')
+const {getAllReviews, createReview, getReviewsByProduct, updateReview, deleteReview} = require('../Controllers/reviewController')
 
 router.get('/', async (req, res, next) => {
     try {
@@ -42,9 +41,35 @@ router.get('/:productId', async (req, res, next) => {
     }
 })
 
-// router.put('/:productId', async (req, res, next) => {
-    // WIP WIP
-// }) 
+router.put('/:reviewId', async (req, res, next) => {
+    try {
+        const { reviewId } = req.params
+        const { userId, product, rating, comment } = req.body
+        const isValid = mongoose.isValidObjectId(reviewId)
+        if(isValid) {
+        const updatedReview = await updateReview(reviewId, userId, product, rating, comment);
+        if(updatedReview.error) throw new Error(updatedReview.error)
+        return res.status(200).json(updatedReview)
+        }
+        return res.status(400).send('Ingrese un id válido')
+    } catch (error) {
+        return res.status(400).send(error.message)
+    }
+}) 
+
+router.delete ('/:reviewId', async (req, res, next) => {
+    try {
+        const { reviewId } = req.params
+        const isValid = mongoose.isValidObjectId(reviewId)
+        if(isValid) {
+            const deletedReview = await deleteReview(reviewId)
+            if(deletedReview.error) throw new Error (deletedReview.error)
+            return res.status(200).json(deletedReview)
+         }
+    } catch (error) {
+        return res.status(400).send(error.message)
+    }
+})
 
 
 
