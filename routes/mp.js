e376c30c3708020ebc1009332da84d51d6655610
danielDeVita <danielitodevita@gmail.com>
2023-03-30 13:04:07 -0300

    back casi que bien

--- routes/mp.js
+++ routes/mp.js
@@ -19,8 +19,8 @@ router.post('/', async (req, res, next) => {
         }),
         back_urls: {
             success: 'http://localhost:5173/paymentStatus',
-            pending: 'https://www.google.com',
-            failure: 'https://www.google.com'
+            pending: 'http://localhost:5173/paymentStatus',
+            failure: 'http://localhost:5173/paymentStatus'
         }
     };
     mercadopago.preferences
@@ -36,82 +36,25 @@ router.post('/', async (req, res, next) => {
 
 router.get('/payment-status', (req, res) => {
     const { payment_id } = req.query;
-    console.log(payment_id)
     mercadopago.payment
         .get(payment_id)
         .then((response) => {
+            console.log(response.body)
             const { status } = response.body;
             if (status === 'approved') {
-                return res.send({ message: 'Payment approved' });
+                return res.send({ message: 'Compra aprobada' });
             } else if (status === 'rejected') {
-                return res.send({ message: 'Payment rejected' });
-            } else if (status === 'pending') {
-                return res.send({ message: 'Payment is being processed' });
+                return res.send({ message: 'Compra rechazada' });
+            } else if (status === 'in_process') {
+                return res.send({ message: 'Compra pendiente' });
             } else {
-                return res.status(500).send({ error: 'Unknown payment status' });
+                return res.status(500).send({ error: 'Sin información del estado de compra' });
             }
         })
         .catch((error) => {
             console.error(error);
-            return res.status(500).send({ error: 'Internal server error' });
+            return res.status(500).send({ error: 'Ha ocurrido un error' });
         });
 });
 
-
-// Handle successful payment
-router.get('/success', (req, res) => {
-    const { product } = req.query;
-    return res.json({ message: `Thank you for your purchase! You have successfully bought ${product}.` });
-});
-
-// Handle failed payment
-router.get('/failure', (req, res) => {
-    const { product } = req.query;
-    return res.status(400).json({ message: 'Payment failed. Please try again later.' });
-});
-
-// Handle pending payment
-router.get('/pending', (req, res) => {
-    const { product } = req.query;
-    return res.json({ message: 'Your payment is being processed.' });
-});
-
-//NOTIFICATION URL, ACA MERCADO PAGO POSTEA DATA DE LA COMPRA (STATUS Y COSAS ASI)
-router.post('/notification', (req, res) => {
-    const { body } = req;
-    // Process the notification
-    console.log(body);
-    res.status(200).end();
-});
-
-
 module.exports = router
-
-/* 
-
-import React, { useEffect, useState } from 'react';
-
-const PaymentStatus = ({ location }) => {
-  const [status, setStatus] = useState('');
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const paymentId = searchParams.get('payment_id');
-
-    fetch(`/payment-status?payment_id=${paymentId}`)
-      .then((response) => response.json())
-      .then((data) => setStatus(data.message))
-      .catch((error) => console.error(error));
-  }, [location.search]);
-
-  return (
-    <div>
-      <h1>Payment Status</h1>
-      <p>{status}</p>
-    </div>
-  );
-};
-
-export default PaymentStatus;
-
-*/
