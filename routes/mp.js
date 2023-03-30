0296107d4f84a2b56424210afd19424dd9d6e832
Julianfmv <julianfmv@gmail.com>
2023-03-29 21:16:23 -0300

    test2

--- routes/mp.js
+++ routes/mp.js
@@ -7,30 +7,30 @@ mercadopago.configure({
     access_token: process.env.ACCESS_TOKEN,
 });
 
-router.get('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
 
     let preference = {
         back_urls: {
-            success: "http://localhost:3001/mp/ok",
-            pending: "",
-            failure: ""
+            success: "http://localhost:3001/feedback",
+            pending: "http://localhost:3001/feedback",
+            failure: "http://localhost:3001/feedback"
         },
-        items: [
-            {
-                title: "nombreProducto",
-                unit_price: 1000,
-                quantity: 1,
-                curreny_id: "ARS"
-            },
-        ],
-        notification_url: ""
+        items: req.body.products.map((item) => ({
+            title: item.descriptionName,
+            unit_price: item.price,
+            quantity: item.quantity,
+            currency_id: 'ARS'
+        })),
+        auto_return:"approved",
     };
 
     mercadopago.preferences
         .create(preference)
         .then(function (response) {
             // En esta instancia deberás asignar el valor dentro de response.body.id por el ID de preferencia solicitado en el siguiente paso
-            res.send(`<a href="${response.body.init_point}">PAGAR</a>`)
+            res.json({
+                id: response.body.id
+            })
         })
         .catch(function (error) {
             console.log(error);
@@ -38,8 +38,12 @@ router.get('/', async (req, res, next) => {
 
 })
 
-router.get('/ok', async(req, res, next)=>{
-    res.send('ok')
+router.get('/feedback', async(req, res, next)=>{
+    res.json({
+        Payment: res.query.payment_id,
+        Status: req.query.status,
+        MerchantOrder: req.query.merchant_order_id
+    })
 })
 
 module.exports = router
